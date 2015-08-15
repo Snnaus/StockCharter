a4947c5580cfa22a18c8fc8ec70bb3fc8dd2ae00
snnaus <nausleyg@gmail.com>
2015-08-15 01:16:03 +0000

    the render is working; but there seems to be a bug with the content of the graph

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('workspaceApp')
-  .controller('MainCtrl', function ($scope, $http) {
+  .controller('MainCtrl', function ($scope, $http, $interval) {
     $scope.stockSearch = {
       Normalized: false,
       NumberOfDays: 15,
@@ -11,16 +11,19 @@ angular.module('workspaceApp')
     
     $scope.stocks = {};
     $scope.addStock = '';
+    $scope.currStocks = [];
     
-    $scope.addElement = function(input, obj){
-      input = input.toUpperCase()
-      obj.Elements.push({
-        Symbol: input,
-        Type: 'price',
-        Params: ['c']
-      });
-      
-      lookupStocks(obj);
+    $scope.addElement = function(input, obj, stocks){
+      if(input){
+        input = input.toUpperCase();
+        obj.Elements.push({
+          Symbol: input,
+          Type: 'price',
+          Params: ['c']
+        });
+        $('#addInput').val('');
+        lookupStocks(obj);
+      }
     };
     
     var lookupStocks = function(search){
@@ -44,4 +47,40 @@ angular.module('workspaceApp')
         
       });
     };
+    
+    var renderGraph = function(){
+      if($scope.stocks.Elements){
+        var input = $scope.stocks;
+        var newStock = input.Elements.map(function(stock){ return stock.Symbol }).filter(function(stock){ return $scope.currStocks.indexOf(stock) === -1 });
+        if(newStock.length > 0){
+          $scope.currStocks = input.Elements.map(function(stock){ return stock.Symbol });
+          var graph = {
+            chart: {
+              type: 'line'
+            },
+            title: {
+              text: "Stock prices over the last 15 days"
+            },
+            xAxis: {
+              categories: input.Elements.map(function(ele){ return ele.Symbol })
+            },
+            yAxis: {
+              title: "Price"
+            },
+            series: input.Elements.map(function(ele){
+              return {
+                name: ele.Symbol,
+                data: ele.DataSeries.close.values
+              };
+            })
+        };
+          $('#container').highcharts(graph);
+        }
+      }
+    };
+    
+    var timer = $interval(renderGraph, 100);
+    $scope.$on('$destroy', function(){
+      $interval.cancel(timer);
+    });
   });
